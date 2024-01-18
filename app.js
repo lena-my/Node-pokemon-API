547cbf9e2b58b9dc39e8a727f1ed437df6145ab4
Helena <helenamiura@gmail.com>
2024-01-18 09:57:05 +0100

    add route with req.params.id

--- app.js
+++ app.js
@@ -12,5 +12,12 @@ const port = 3000;
 // Lorsque quelqu'un accède à la racine, le serveur répond avec le message "Hello Express! 😃"
 app.get('/', (req, res) => res.send('Hello Express 2! 😃'));
 
+app.get('/api/pokemon/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Vous avez demandé le pokémon ${id}`)
+});
+
+
+
 // Lance le serveur sur le port spécifié et affiche un message dans la console indiquant que le serveur est en cours d'exécution
 app.listen(port, () => console.log(`Notre application Node est démarrée sur http://localhost:${port}`));
