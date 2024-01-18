1f72ff6e28659d257f9757cd7d5351cbc811976c
Helena <helenamiura@gmail.com>
2024-01-18 09:37:56 +0100

    nodemon and express install

---
+++ app.js
@@ -0,0 +1,16 @@
+// Importe le module Express, un framework web pour Node.js
+const express = require('express');
+const { start } = require('repl');
+
+// Crée une instance de l'application Express. serveur web où l'api rest va fonctionner
+const app = express();
+
+// Définit le numéro de port sur lequel le serveur écoutera
+const port = 3000;
+
+// Définit une route pour la racine de l'application ('/')
+// Lorsque quelqu'un accède à la racine, le serveur répond avec le message "Hello Express! 😃"
+app.get('/', (req, res) => res.send('Hello Express 2! 😃'));
+
+// Lance le serveur sur le port spécifié et affiche un message dans la console indiquant que le serveur est en cours d'exécution
+app.listen(port, () => console.log(`Notre application Node est démarrée sur http://localhost:${port}`));
