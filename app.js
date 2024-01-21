9645c9632f2f89c08f8372b339bcdc239fc1f336
Helena <helenamiura@gmail.com>
2024-01-22 00:55:35 +0100

    modifs api

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');// Importe le module Express, un framework web pour Node.js
 const morgan = require('morgan'); //importe morgan
 const favicon = require('serve-favicon'); // import favicon
-const bodyParser = require('body-parser'); 
-const PokemonModel = require('./src/models/pokemon'); // importe le modèle pokemon
-const sequelize = require('./src/db/sequelize')
+const bodyParser = require('body-parser');
+const sequelize = require('./src/db/sequelize');
 
 const app = express();// Crée une instance de l'application Express. serveur web où l'api rest va fonctionner
 const port = 3000; // Définit le numéro de port sur lequel le serveur écouter
@@ -16,6 +15,9 @@ app
 sequelize.initDb();
 
 //ici nous placerons nos futurs points de terminaison.
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
 
 // Lance le serveur sur le port spécifié et affiche un message dans la console indiquant que le serveur est en cours d'exécution
 app.listen(port, () => console.log(`Notre application Node est démarrée sur http://localhost:${port}`));
