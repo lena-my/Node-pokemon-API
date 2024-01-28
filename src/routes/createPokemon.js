bc7f9bf26d14e022f47ccdbac59ce544202f65c3
helenamiura <helenamiura@gmail.com>
2024-01-28 18:29:50 +0100

    add status validator

--- src/routes/createPokemon.js
+++ src/routes/createPokemon.js
@@ -1,3 +1,4 @@
+const { ValidationError } = require('sequelize')
 const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
@@ -8,7 +9,10 @@ module.exports = (app) => {
         res.json({ message, data: pokemon })
       })
       .catch(error => {
-        const message = `Le pokemon n'a pas pu être ajouté. Réessayez dans quelques instants.`;
+        if(error instanceof ValidationError){
+          return res.status(400).json({message: error.message, data: error})
+        }
+        const message = `Le pokemon n'a pas pu être ajouté. Réessayez dans quelques instants.`
         res.status(500).json({ message, data: error})
       })
   })
